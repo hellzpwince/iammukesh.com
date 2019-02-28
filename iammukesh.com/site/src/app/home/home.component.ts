de3d1ed03c56bcec8a07fed0089d58a971486776
hellzpwince <admin@iammukesh.com>
2019-03-01 02:32:10 +0530

    final touch

--- iammukesh.com/site/src/app/home/home.component.ts
+++ iammukesh.com/site/src/app/home/home.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
-import { Router, Route, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { ProjectService } from '../project-service';
 declare var previousTestimonial: any;
 declare var nextTestimonial: any;
 declare var basicInitialization: any
@@ -21,7 +19,7 @@ export class HomeComponent implements OnInit {
                              a variety of topics that include a video, 
                              writeup, and github repo.`;
 
-  constructor(private router: Router, private meta: Meta, private title: Title, private route: ActivatedRoute,db: AngularFirestore) {
+  constructor( private meta: Meta, private title: Title,db: AngularFirestore) {
     this.title.setTitle('Mukesh Joshi | Developer & Digital Consultant');
     this.meta.addTag({ name: 'description', content: 'Mukesh Joshi is a web consultant providing web services to various companies and businesses. Click to know more about him' });
     this.meta.addTag({ name: 'author', content: 'Mukesh Joshi' });
@@ -39,9 +37,6 @@ export class HomeComponent implements OnInit {
   }
   nextTestimonial() {
     new nextTestimonial();
-  }
-  routeProject() {
-    this.router.navigate(['/project']);
   }
 
 
--- iammukesh.com/site/src/environments/environment.prod.ts
+++ iammukesh.com/site/src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-   
+    apiKey: "AIzaSyCK3vM-FxQ9Q-UcpHhwolfebwbzrq-J6MI",
+    authDomain: "iammukesh-com.firebaseapp.com",
+    databaseURL: "https://iammukesh-com.firebaseio.com",
+    projectId: "iammukesh-com",
+    storageBucket: "",
+    messagingSenderId: "766722374260"
 }
 };
