cf748c07d97597c4c2370d144d84ac66ffb70dbd
Mukesh Joshi <admin@iammukesh.com>
2019-03-01 02:39:22 +0530

    Update environment.ts

--- iammukesh.com/site/src/environments/environment.ts
+++ iammukesh.com/site/src/environments/environment.ts
@@ -5,16 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCK3vM-FxQ9Q-UcpHhwolfebwbzrq-J6MI",
-    authDomain: "iammukesh-com.firebaseapp.com",
-    databaseURL: "https://iammukesh-com.firebaseio.com",
-    projectId: "iammukesh-com",
-    storageBucket: "",
-    messagingSenderId: "766722374260"
+    
 }
 };
 
 /*
+* Add your Firebase Details here
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
