61a4447e57d8ac832f38de11db4882725b572fe1
hellzpwince <admin@iammukesh.com>
2019-03-01 02:45:04 +0530

    update

--- iammukesh.com/site/src/environments/environment.ts
+++ iammukesh.com/site/src/environments/environment.ts
@@ -5,7 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    
+    apiKey: "AIzaSyCK3vM-FxQ9Q-UcpHhwolfebwbzrq-J6MI",
+    authDomain: "iammukesh-com.firebaseapp.com",
+    databaseURL: "https://iammukesh-com.firebaseio.com",
+    projectId: "iammukesh-com",
+    storageBucket: "",
+    messagingSenderId: "766722374260"
 }
 };
 
