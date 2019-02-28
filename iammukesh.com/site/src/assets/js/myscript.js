26f4818fc351e7196344f5dd2a8a932d9aaa00ea
hellzpwince <admin@iammukesh.com>
2019-03-01 03:03:35 +0530

    favicon & css adjustments

--- iammukesh.com/site/src/assets/js/myscript.js
+++ iammukesh.com/site/src/assets/js/myscript.js
@@ -40,7 +40,7 @@ function homePageInitialization(){
         });
         var typed = new Typed("#typed", {
             // Waits 1000ms after typing "First"
-            strings: ["YOUR HOST ^3000", "I DEVELOP 💻/📱 APPS  ^3000", "I'M A DESIGNER TOO 😉 ^3000", "I ❤️ MAKING VIDEOS ^3000", "LOOKING FOR HELP? ^2000", "PING ME NOW 😄 ^3000"],
+            strings: ["YOUR HOST, MUKESH JOSHI ^3000", "I DEVELOP 💻/📱 APPS  ^3000", "I'M A DESIGNER TOO 😉 ^3000", "I ❤️ MAKING VIDEOS ^3000", "LOOKING FOR HELP? ^2000", "PING ME NOW 😄 ^3000"],
             typeSpeed: 40,
             smartBackspace: true,
             backSpeed: 30,
